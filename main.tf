3ab84ce4252de7a454ac4ddaaa09f5f528e99262
Adrián Muñoz Pérez <57462617+Adrian-98@users.noreply.github.com>
2023-04-11 11:33:17 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,8 +50,8 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  label        = "devops"
-  billable     = "true"
+  department   = "devops"
+  billable.    = "true"
 
   boot_disk {
     initialize_params {
