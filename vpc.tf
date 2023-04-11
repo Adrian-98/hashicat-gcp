5a33e8d48be48d62db0564e6b501636d3ccacc4f
Adrián Muñoz Pérez <57462617+Adrian-98@users.noreply.github.com>
2023-04-11 12:16:25 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,14 @@
-subnets = [
+module "network" {
+  source  = "app.terraform.io/TerraF-LAb/network/google"
+  version = "0.0.0"
+  # insert required variables here
+  subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
 ]
+}
+
+
